ce27eaa0d48a22e6665d2c2766e84a409d768594
vitorio <vitorioaugusto09072003@gmail.com>
2025-06-22 17:58:49 -0300

    Q - 03 Ts

---
+++ Q -03.ts
@@ -0,0 +1,18 @@
+// Questão 03
+let a: number = 10;
+let b: number = 5;
+let soma: number = a + b;
+console.log("Soma:", soma);
+
+let nome: string = "Vitório";
+let saudacao: string = "Olá, " + nome + "!";
+console.log(saudacao); 
+
+let aprovado: boolean = true;
+console.log("Aprovado?", aprovado); 
+
+let notaFinal: number | undefined = undefined;
+console.log("Nota Final:", notaFinal); 
+
+notaFinal = 9.5;
+console.log("Nota Final Atualizada:", notaFinal);
