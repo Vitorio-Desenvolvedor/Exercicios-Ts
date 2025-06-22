b4e84ab74bddf4081f4cdcdabfdee90d4c20d47e
vitorio <vitorioaugusto09072003@gmail.com>
2025-06-22 17:48:53 -0300

    Q -2 Ts

---
+++ Q -02.ts
@@ -0,0 +1,13 @@
+// Questão - 02
+let idade: number = 25;
+idade = 26;
+console.log("let idade:", idade);
+
+const nome: string = "Vitório";
+
+console.log("const nome:", nome);
+
+var curso: string = "TypeScript";
+curso = "JavaScript";
+console.log("var curso:", curso);
+
