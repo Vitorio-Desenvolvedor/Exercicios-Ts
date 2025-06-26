9b386733f8f335601c6ab62caf01a1e088db5ce6
VITORIO AUGUSTO LIMA NASCIMENTO <a95190787@rn.senac.local>
2025-06-26 10:49:16 -0300

    Questões resolvidas

---
+++ Q -05.ts
@@ -0,0 +1,3 @@
+let cidade = "Mossoró";
+
+console.log(`Sua cidade é ${cidade}`) //O TypeScript olha para o valor "Mossoro" e deduz que cidade é uma string.
---
+++ Q -06.ts
@@ -0,0 +1,5 @@
+let valorTexto: string = "100";
+let  converter: number = parseInt(valorTexto);
+
+console.log(`Valor tipo texto ${valorTexto}`);
+console.log(`Valor convertido ${converter}`);
